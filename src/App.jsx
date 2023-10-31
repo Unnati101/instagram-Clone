f15b5feb098fd3c8682bdc079031ac7831b68f92
Unnati Rastogi <rastogiunnati25@gmail.com>
2023-10-31 21:52:28 +0530

    added sign in function

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from 'react';
 import logo from './assets/pngegg.png';//25:00
 import './App.css'
 import Post from './Post.jsx';
-import { db } from './firebase.js';
+import { auth, db } from './firebase.js';
 import { makeStyles } from '@material-ui/core/styles';
-import { Modal } from '@material-ui/core';
-
+import { Input, Modal } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 
 function getModalStyle() {
@@ -37,7 +37,31 @@ function App() {
   const [modalStyle] = useState(getModalStyle);
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] =useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser]=useState(null);
 
+  useEffect(()=>{
+  const unsubscribe=auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        //user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+        
+       
+      }else{
+        setUser(null);
+      }
+    })
+    return ()=>{
+      //perform some cleanup actions
+      unsubscribe();
+    }
+  },[user, username]);
+
+ 
   // {]
   //   username:"unnati",
   //   caption: "I'am making an instagram clone",
@@ -64,8 +88,29 @@ function App() {
     })
   }, []);
 
+ const signUp=(event) => {
+  event.preventDefault();
+  auth
+  .createUserWithEmailPassword(email,password)
+  .then((authUser)=>{
+    return authUser.user.updateProfile
+    displayName: username;
+  })
+ 
 
+  .catch((error)=>alert(error.message));
+  setOpen(false);
+ }
 
+ const signIn=(event)=>{
+
+  event.preventDefault();
+  auth
+  .signInWithEmailAndPassword(email,password)
+  .catch((error)=>alert(error.message));
+
+  setOpenSignIn(false);
+ }
   return (
     <div className="App">
       <Modal
@@ -74,17 +119,76 @@ function App() {
 
       >
         <div style={modalStyle} className={classes.paper}>
-          <h2>I am a modal</h2>
+          <form className="app_sign">
+          <center>
+            <img
+              className="app-headerlogo"
+              src={logo}
+              />
+              </center>
+              <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
+              />
+               <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+              />
+              <Button type="submit" onClick={signUp}>Sign Up</Button>
+          </form>
+         
+          
         </div>
 
       </Modal>
+      <Modal
+      open={openSignIn}
+      onClose={()=>setOpenSignIn(false)}
+>
+<div style={modalStyle} className={classes.paper}>
+  <form className="app_signup">
+    <center>
+   <img
+   className="app-headerlogo"
+   src={logo}
+   />
+    </center>
+    <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e)=> setEmail(e.target.value)}
+              />
+               <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e)=> setPassword(e.target.value)}
+              />
+               <Button type="submit" onClick={signIn}>Sign In</Button>
 
-      <div className="App-header">
+  </form>
+  </div>
+  </Modal>
+<div>
+    
         <img
           className="app-headerlogo"
           src={logo}
         />
       </div>
+      {user?(
+        <Button onClick={()=>auth.signOut()}>Logout</Button>
+      ):(
+        <div className="app_loginContainer">
+          <Button onClick={()=>setOpenSignIn(true)}>Sign In</Button>
+          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+</div>
+      )}
       <h1>Hello let`s build a Instagram clone</h1>
       {
         posts.map(({ id, post }) => (
